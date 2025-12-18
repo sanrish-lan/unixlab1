5c6d0768b47e562c193172c8a67eb596acc7d284
Roman Kuznetsov <ooaa3095@gmail.com>
2025-12-18 18:40:46 +0700

    oslab3.1

--- oslab3/module_1_part.c
+++ oslab3/module_1_part.c
@@ -7,16 +7,16 @@ MODULE_AUTHOR("Kuznetsov Roman");
 MODULE_DESCRIPTION("OSlab3 part 1");
 MODULE_INFO('Date', "2025-12-18")
 
-static int __init tsu_init(void)
+static int __init module_init(void)
 {
     pr_info("Welcome to the Tomsk State University\n");
     return 0;
 }
 
-static void __exit tsu_exit(void)
+static void __exit module_exit(void)
 {
     pr_info("Tomsk State University forever!\n");
 }
 
-module_init(tsu_init);
-module_exit(tsu_exit);
+module_init(module_init);
+module_exit(module_exit);
